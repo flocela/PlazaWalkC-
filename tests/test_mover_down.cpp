87a3010d262c766d3adea49a3575fe3be2a9af61
Flo B Jones <flocela@gmail.com>
2023-12-30 17:11:25 -0800

    Changed comment on test_mover.cpp. The new positions are down, then in the direction that the box is traveling (down) diagonal down and right, then diagonal down and left.

--- tests/test_mover_down.cpp
+++ tests/test_mover_down.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //      first position is Position(a, b+1). Corresponding to moving down.
 //      second position is Position(a-1, b+1).
 //      third position is Position(a+1, b+1).
-TEST_CASE("Should return next positions, which are down, then the diagonal position down and left, then the diagonal position down and right.")
+TEST_CASE("Should return next positions, which are down, then the diagonal position down and right, then the diagonal position down and left.")
 {
     // note0's type is 4, which means it arrives at Position(5, 5);
     BoxNote note0{4, Position{5, 5}, Position{5, 4}, std::chrono::high_resolution_clock::now()};
