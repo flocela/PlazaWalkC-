cc98c6d0c3ef822418989ec39e9c2f3ed82d5b6b
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2024-04-30 14:32:34 -0700

    Explicitly made BoradProxy copy constructor and move constructor default. Deleted assignment operators. And moved delete method to public!!

--- src/BoardProxy.h
+++ src/BoardProxy.h
@@ -10,13 +10,21 @@ class BoardProxy
 
     // Calls Board's sendChanges method.
     void sendChanges();
-    
-    friend class Board;
 
+    // Board is the only class that can construct a Board.    
+    friend class Board;
+    
+    BoardProxy() = delete;
+    BoardProxy(const BoardProxy& o) = default;
+    BoardProxy(BoardProxy&& o) noexcept = default;
+    BoardProxy& operator=(const BoardProxy& o) = delete;
+    BoardProxy& operator=(BoardProxy&& o) noexcept = delete;
+    ~BoardProxy() noexcept = default;
 
     private:
 
     BoardProxy(Board& board);
+
     Board& _board;
 };
 
