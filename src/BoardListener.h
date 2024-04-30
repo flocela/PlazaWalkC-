82c02281a9673e2217f282a0fcfccd1e824c4452
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2024-04-30 13:21:26 -0700

    Changed comments in BoardListener.

--- src/BoardListener.h
+++ src/BoardListener.h
@@ -5,13 +5,14 @@
 #include <unordered_set>
 #include "BoxInfo.h"
 #include "Drop.h"
-#include "SpotType.h"
 
-// Receives 1) changes to Spots from Board and 2) the current state of the Boxes. Changes to Spots are in the form of an unordered_set<Drop>. changedDrops only contains the Drops that have changed since the last time receiveChanges() was called. The current state of the Boxes is in the form of an unordered_map<int, BoxInfo>. All Boxes are included, even Boxes that have not entered the Board yet or have been removed because they reached their final destination.
+/*
+Receives from Board 1) changes to Spots and 2) the current state of the Boxes. Changes to Spots are in the form of an unordered_set<Drop>. changedDrops only contains the Drops that have changed since the last time receiveChanges() was called. The current state of the Boxes is in the form of an unordered_map<int, BoxInfo>, which is the Boxes per their id. All Boxes are included, even Boxes that have not entered the Board yet or have been removed because they reached their final destination.
+*/
 class BoardListener
 {
 
-public:
+    public:
 
     virtual void receiveChanges(std::unordered_set<Drop> changedDrops,
                                 std::unordered_map<int, BoxInfo> boxesPerId) = 0;
